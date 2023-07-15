335d9783ecbf1f06794a4559983b676e72b3326f
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-07-15 20:31:37 +0000

    Betty removed spaces

--- shell.c
+++ shell.c
@@ -9,48 +9,59 @@
 
 extern char **environ;
 
+/**
+ * display_prompt - Display the shell prompt.
+ */
 void display_prompt(void)
 {
-    printf(">> ");
+	printf(">> ");
 }
 
+/**
+ * read_command - Read the user's command from standard input.
+ * @command: The command buffer.
+ */
 void read_command(char *command)
 {
-    fgets(command, MAX_COMMAND_LENGTH, stdin);
+	fgets(command, MAX_COMMAND_LENGTH, stdin);
 
-    command[strcspn(command, "\n")] = '\0';
+	command[strcspn(command, "\n")] = '\0';
 }
 
+/**
+ * execute_command - Execute the given command.
+ * @command: The command to execute.
+ */
 void execute_command(const char *command)
 {
-    if (access(command, X_OK) == 0)
-    {
-        pid_t pid = fork();
+	if (access(command, X_OK) == 0)
+	{
+		pid_t pid = fork();
 
-        if (pid == -1)
-        {
-            perror("fork");
-        }
-        else if (pid == 0)
-        {
+		if (pid == -1)
+		{
+			perror("fork");
+		}
+		else if (pid == 0)
+		{
 
-            char *argv[2];
-            argv[0] = (char *)command;
-            argv[1] = NULL;
-            execve(command, argv, environ);
-            perror("execve");
-            exit(EXIT_FAILURE);
-        }
-        else
-        {
+			char *argv[2];
+			argv[0] = (char *)command;
+			argv[1] = NULL;
+			execve(command, argv, environ);
+			perror("execve");
+			exit(EXIT_FAILURE);
+		}
+		else
+		{
 
-            wait(NULL);
-        }
-    }
-    else
-    {
-        printf("Command not found: %s\n", command);
-    }
+			wait(NULL);
+		}
+	}
+	else
+	{
+		printf("Command not found: %s\n", command);
+	}
 }
 
 
