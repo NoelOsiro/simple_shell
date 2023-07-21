f5900282d395c250a40e2e433538c647b51a8eca
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-07-21 16:30:39 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,5 +1,12 @@
 #include "my_shell.h"
 
+/**
+ * @brief Main function that starts the shell.
+ *
+ * @param ac Number of command-line arguments.
+ * @param argv Array of command-line argument strings.
+ * @return Returns 0 on successful completion.
+ */
 int main(int ac, char **argv)
 {
     (void)ac;
