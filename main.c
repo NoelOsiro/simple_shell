5995401acaeee0bda6ce17f233ac044c7ec9888f
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-07-21 16:24:49 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include "my_shell.h"
 
-int main(int ac, char **argv) {
-    /* declaring void variables */
+int main(int ac, char **argv)
+{
     (void)ac;
     (void)argv;
-
     run_shell();
     return 0;
 }
