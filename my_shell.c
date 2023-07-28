87538c7343846953f3d76f3811f8060f92cc6aa7
Noel Otieno Osiro <105745954+NoelOsiro@users.noreply.github.com>
2023-07-28 16:12:12 +0000

    Betty

--- my_shell.c
+++ my_shell.c
@@ -2,91 +2,131 @@
 #include "commands.h"
 
 /**
- * run_shell - Run the interactive shell.
+ * execute_command - Prompt the user for input and execute the entered command.
  *
- * This function implements a simple interactive shell that continuously prompts
- * the user for commands and executes them. The shell reads input from the standard
- * input (stdin) and parses each line into individual tokens (words) using the specified delimiter.
- * It then forks a new process to execute the command using the `execvp()` function,
- * which searches for the command in the directories listed in the PATH environment variable.
- *
- * The function uses the `getline()` function to read input lines from the user.
- * It allocates memory for each command token and ensures proper memory deallocation
- * after command execution. The shell waits for the command to complete using the `waitpid()`
- * function before showing the next prompt for user input.
- *
- * The shell will terminate when the user presses the end-of-file (EOF) character
- * (usually Ctrl + D on Unix-based systems) or if there's an error in memory allocation or
- * command execution.
+ * @param prompt The prompt to display to the user.
+ * Returns: 0 on successful execution, -1 on failure.
  */
+int execute_command(const char *prompt)
+{
+	char *lineptr = NULL;
+	size_t n = 0;
+	ssize_t nread;
 
+	printf("%s", prompt);
+	nread = getline(&lineptr, &n, stdin);
+
+	if (nread == -1)
+	{
+		printf("Ski Out....\n");
+		free(lineptr);
+		return (-1);
+	}
+
+	char **argv = tokenize_input(lineptr);
+
+	if (execute_cmd(argv) == -1)
+	{
+		perror("Execution error");
+		free(lineptr);
+		free_tokens(argv);
+		return (-1);
+	}
+
+	free(lineptr);
+	free_tokens(argv);
+	return (0);
+}
+
+/**
+ * tokenize_input - Tokenize the input line.
+ *
+ * @lineptr: The input line to tokenize.
+ * Returns: a dynamically allocated array of command tokens.
+ */
+char **tokenize_input(char *lineptr)
+{
+	const char *delim = " \n";
+	char *token;
+	int num_tokens = 0;
+
+	char *lineptr_copy = strdup(lineptr);
+	if (lineptr_copy == NULL)
+	{
+		perror("tsh: memory allocation error");
+		return (NULL);
+	}
+
+	token = strtok(lineptr_copy, delim);
+	while (token != NULL)
+	{
+		num_tokens++;
+		token = strtok(NULL, delim);
+	}
+
+	char **argv = malloc(sizeof(char *) * (num_tokens + 1));
+	if (argv == NULL)
+	{
+		perror("tsh: memory allocation error");
+		free(lineptr_copy);
+		return (NULL);
+	}
+
+	token = strtok(lineptr, delim);
+	for (int i = 0; token != NULL; i++)
+	{
+		argv[i] = strdup(token);
+		token = strtok(NULL, delim);
+	}
+	argv[num_tokens] = NULL;
+
+	free(lineptr_copy);
+	return (argv);
+}
+
+/**
+ * execute_cmd - Execute the given command using execvp.
+ * @argv: The command and its arguments as an array of strings.
+ * Returns: 0 on successful execution, -1 on failure.
+ */
+int execute_cmd(char **argv)
+{
+	pid_t pid = fork();
+	if (pid < 0)
+	{
+		perror("Fork error");
+		return (-1);
+	} else if (pid == 0)
+	{
+		execvp(argv[0], argv);
+		return (-1);
+	} else
+	{
+		int status;
+		waitpid(pid, &status, 0);
+		return (0);
+	}
+}
+
+/**
+ * free_tokens - Free the allocated memory for command tokens.
+ * @argv: The array of command tokens.
+ */
+void free_tokens(char **argv)
+{
+	for (int i = 0; argv[i] != NULL; i++)
+	{
+		free(argv[i]);
+	}
+	free(argv);
+}
+
+/**
+ * run_shell - Run the interactive shell.
+ * This function continuously prompts the user for commands and executes them.
+ */
 void run_shell(void)
 {
 	char *prompt = "(Azubi)$ ";
-	char *lineptr = NULL, *lineptr_copy = NULL;
-	size_t n = 0;
-	ssize_t nread;
-	const char *delim = " \n";
-	char **argv;
-	int i;
-	int num_tokens = 0;
-	char *token;
-	pid_t pid;
-
-	while (1)
-	{
-		printf("%s", prompt);
-		nread = getline(&lineptr, &n, stdin);
-		if (nread == -1)
-		{
-			printf("Ski Out....\n");
-			break;
-		}
-		lineptr_copy = malloc(sizeof(char) * nread);
-		if (lineptr_copy == NULL)
-		{
-			perror("tsh: memory allocation error");
-			break;
-		}
-		strcpy(lineptr_copy, lineptr);
-		token = strtok(lineptr_copy, delim);
-		while (token != NULL)
-		{
-			num_tokens++;
-			token = strtok(NULL, delim);
-		}
-		num_tokens++;
-		argv = malloc(sizeof(char *) * num_tokens);
-		token = strtok(lineptr_copy, delim);
-		for (i = 0; token != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * (strlen(token) + 1));
-			strcpy(argv[i], token);
-			token = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-		pid = fork();
-		if (pid < 0)
-		{
-			perror("Fork error");
-		}
-		else if (pid == 0)
-		{
-			execvp(argv[0], argv);
-			perror("Execution error");
-			exit(1);
-		}
-		else
-		{
-			int status;
-			waitpid(pid, &status, 0);
-		}
-		for (i = 0; i < num_tokens; i++)
-		{
-			free(argv[i]);
-		}
-		free(argv);
-		free(lineptr_copy);
-	}
-	free(lineptr);
+	while (execute_command(prompt) == 0);
 }
